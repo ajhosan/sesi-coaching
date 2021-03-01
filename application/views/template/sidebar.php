6fecc3cb60e02eafc46e416153981487622b25b7
ajhosan <alexjhosan19@gmail.com>
2021-03-01 21:53:40 +0700

    Template

---
+++ application/views/template/sidebar.php
@@ -0,0 +1,54 @@
+<!-- Page Wrapper -->
+<div id="wrapper">
+
+    <!-- Sidebar -->
+    <ul class="navbar-nav sidebar sidebar-dark accordion" id="accordionSidebar" style="background-color: #2043E5;">
+
+        <!-- Sidebar - Brand -->
+        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <div class="sidebar-brand-icon rotate-n-15">
+                <i class="fas fa-building"></i>
+            </div>
+            <div class="sidebar-brand-text mx-3">Sesi Coaching</div>
+        </a>
+
+        <!-- Divider -->
+        <hr class="sidebar-divider my-0">
+
+        <!-- Nav Item - Dashboard -->
+        <li class="nav-item active">
+            <a class="nav-link" href="index.html">
+                <i class="fas fa-fw fa-tachometer-alt"></i>
+                <span>Goals & Action Plan</span></a>
+        </li>
+
+        <!-- Divider -->
+        <hr class="sidebar-divider">
+
+        <!-- Heading -->
+        <div class="sidebar-heading">
+            Profil
+        </div>
+
+        <li class="nav-item">
+            <a class="nav-link" href="index.html">
+                <i class="fas fa-fw fa-id-card"></i>
+                <span>Edit Profil</span></a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="index.html" style="color: red;">
+                <i class="fas fa-sign-out-alt fa-sm fa-fw" style="color: red;"></i>
+                <span>Logout</span></a>
+        </li>
+
+        <!-- Divider -->
+        <hr class="sidebar-divider d-none d-md-block">
+
+        <!-- Sidebar Toggler (Sidebar) -->
+        <div class="text-center d-none d-md-inline">
+            <button class="rounded-circle border-0" id="sidebarToggle"></button>
+        </div>
+
+    </ul>
+    <!-- End of Sidebar -->
